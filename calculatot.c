969db599e224760cbdadf0d3c00765b459d41ac0
Barasha <barashad826@gmail.com>
2021-06-10 19:18:49 +0530

    version 1

---
+++ calculatot.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("adding two nom.\n");
+    scanf("%d\n", &a);
+    scanf("%d\n", &b);
+    printf("the result is %d\n", a + b);
+    return 0;
+}
---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the two numbers:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("c=%d+%d=%d",a,b,c);
+    return 0;
+}
